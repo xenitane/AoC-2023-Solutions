79e99574bbcee7a45b8116b3937c5d01437000ca
Tushar Jain | xenitane <tushar01.tjdsk@gmail.com>
2023-12-09 23:34:22 +0530

    added 2b

---
+++ sol_2_b.cpp
@@ -0,0 +1,61 @@
+/**
+ * @file sol_2_b.cpp
+ * @author Xenitane
+ * @date 2023-12-09
+ *
+ * @copyright Copyright (c) 2023
+ *
+ * @brief read every line, remove the "Game <gameid>:" from the start, extract the info for individual sub game, maintain a count for maximum amount of red, blue and green you have encountered in each sub game and add their product to the result
+ */
+
+#include <bits/stdc++.h>
+
+namespace std {
+	vector<string> split(const string &str, char d = ' ') {
+		vector<string> res(0);
+		size_t		   p = 0, n = str.size();
+		string		   temp = ""s;
+		while (p < n) {
+			if (str[p] == d) [[unlikely]] {
+				res.push_back(temp);
+				temp = ""s;
+			} else [[likely]]
+				temp.push_back(str[p]);
+			p++;
+		}
+		res.push_back(temp);
+		return res;
+	}
+} // namespace std
+
+void solve(int t) noexcept {
+	long long res{};
+	for (std::string str; getline(std::cin, str);) {
+		struct local_data {
+			int r, g, b;
+			local_data() : r(0), g(0), b(0) {}
+			void add(int r, int g, int b) { this->r = std::max(this->r, r), this->g = std::max(this->g, g), this->b = std::max(this->b, b); }
+			int	 fin() { return this->r * this->g * this->b; }
+		} data;
+		str							  = str.substr(str.find(":") + 1);
+		std::vector<std::string> pool = std::split(str, ';');
+		while (!pool.empty()) {
+			std::vector<std::string> sub_pool = std::split(pool.back(), ',');
+			int						 r{}, g{}, b{};
+			while (!sub_pool.empty()) {
+				std::istringstream ss(sub_pool.back());
+				int				   c;
+				ss >> c;
+				ss >> sub_pool.back();
+				if (sub_pool.back() == "red") r = c;
+				if (sub_pool.back() == "green") g = c;
+				if (sub_pool.back() == "blue") b = c;
+				data.add(r, g, b);
+				sub_pool.pop_back();
+			}
+			pool.pop_back();
+		}
+		res += data.fin();
+	}
+	std::cout << res;
+}
